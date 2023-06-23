fb444d08c730386c9940e2ff16fdfcff70c9aea4
Yaseen Khan <91598415+kkhanyaseen@users.noreply.github.com>
2023-06-23 20:54:53 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -530,7 +530,7 @@ if BASE_URL:
     Popen(
         f"gunicorn web.wserver:app --bind 0.0.0.0:{BASE_URL_PORT} --worker-class gevent", shell=True)
 
-srun(["qbittorrent-nox", "-d", f"--profile={getcwd()}"])
+srun(["qolora", "-d", f"--profile={getcwd()}"])
 if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
         pass
